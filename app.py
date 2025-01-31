f70704c8d590744ad649d608f56f43a875d102c3
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-01-30 19:29:01 -0500

    Feat: Adicionar endpoint e função para deletar produtos

--- app.py
+++ app.py
@@ -18,11 +18,22 @@ class Product(db.Model):
 def add_product():
     data = request.json
     if 'name' in data and 'price' in data:
-        product = Product(name=data['name'], price=data['price'], description=data.get('description', ''))
+        product = Product(
+            name=data['name'], price=data['price'], description=data.get('description', ''))
         db.session.add(product)
         db.session.commit()
         return jsonify({'message': 'Product added successfully'})
     return jsonify({'message': 'Invalid product data'}), 400
+
+
+@app.route('/api/products/delete/<int:product_id>', methods=['DELETE'])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({'message': 'Product deleted successfully'})
+    return jsonify({'message': 'Product not found'}), 404
 
 
 @app.route('/')
